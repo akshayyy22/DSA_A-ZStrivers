dbe6eb512d65e314a48efec974c29c20bf15d2ea
akshayyy22 <akshaypersonalstudentid@gmail.com>
2024-06-23 22:19:24 +0530

    3rd Day Linked List

--- Linked-List/Lec 2: Learn Doubly LinkedList/10.java
+++ Linked-List/Lec 2: Learn Doubly LinkedList/10.java
@@ -1,1 +1,28 @@
 // Delete the middle node of LL
+
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        // Check if the list is empty or has only one element
+        if (head == null || head.next == null) {
+            return null; // If there is only one element, return null
+        }
+
+        ListNode hare = head;
+        ListNode tortoise = head;
+
+        // Move hare by 2 steps initially
+        hare = hare.next.next;
+        
+        // Move hare and tortoise until hare reaches the end
+        while (hare != null && hare.next != null) {
+            hare = hare.next.next;
+            tortoise = tortoise.next;
+        }
+
+        // Delete the middle node
+        ListNode middle = tortoise.next;
+        tortoise.next = middle.next;
+
+        return head;
+    }
+}
---
+++ Linked-List/Lec 2: Learn Doubly LinkedList/6.java
@@ -0,0 +1,29 @@
+// Length of Loop in LL
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null; // Return null if there is no cycle
+        }
+
+        ListNode hare = head;
+        ListNode tortoise = head;
+
+        // First step: Determine if a cycle exists
+        while (hare != null && hare.next != null) {
+            hare = hare.next.next;
+            tortoise = tortoise.next;
+            if (hare == tortoise) { // Cycle detected
+                // Second step: Find the start of the cycle
+                tortoise = head;
+                while (tortoise != hare) {
+                    tortoise = tortoise.next;
+                    hare = hare.next;
+                }
+                return hare; // or return tortoise, as both are now at the cycle 
+            }
+        }
+
+        return null; // No cycle found
+    }
+}
